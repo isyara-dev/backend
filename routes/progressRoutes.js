67c7e0d98e51a22e2569f97b2541500281875ac5
Rizka <rizkaal874@upi.edu>
2025-05-20 11:18:29 +0700

    Refactor leaderboard and progress controllers; remove letter routes and implement hand routes

--- routes/progressRoutes.js
+++ routes/progressRoutes.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import * as progressController from '../controllers/progressController.js';
-import verifyUser from '../middlewares/verifyUser.js';
+// import verifyUser from '../middlewares/verifyUser.js';
 
 const router = express.Router();
 
 // All routes are protected
-router.use(verifyUser);
+// router.use(verifyUser);
 
-router.get('/', progressController.getUserProgress);
 router.post('/', progressController.updateProgress);
-router.get('/:letter_id', progressController.getLetterProgress);
+router.get('/sub', progressController.getSubProgress);
+router.get('/module/:languageId/:userId', progressController.getModuleProgress);
+router.get('/language/:userId', progressController.getLanguageProgress);
 
 export default router; 
